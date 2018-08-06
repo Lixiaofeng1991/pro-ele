8ae3389c16b5cb9215fc23aa1300ff678841161f
Lixiaofeng1991 <746404904@qq.com>
2018-08-06 19:52:00 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
   <div id="app">
    
     <router-view/>
+    <div class="tabs">
+      <router-link to="/home" class="tab">首页</router-link>
+      <router-link to="/discover" class="tab">发现</router-link>
+      <router-link to="/indent" class="tab">订单</router-link>
+      <router-link to="/mine" class="tab">我的</router-link>
+    </div>
+   
   </div>
 </template>
 
@@ -11,6 +18,18 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.tabs{
+  width:100%;
+  height: 44px;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  display: flex;
+  border-top: 1px solid #999;
+}
+.tab{
+  flex: 1;
+  text-align: center
+}
 </style>
